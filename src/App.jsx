2a47e2e51e9251471f5fec59522cd90f930134bd
Luong Minh Nhat <luongminhnhat1604@gmail.com>
2025-08-04 16:15:26 +0700

    Added handle user input logic

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,26 @@ import Header from "./components/Header";
 import UserInput from "./components/Userinput";
 
 function App() {
+  const [investmentObj, setInvestmentObj] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 3600,
+    expectedReturn: 6,
+    duration: 10,
+  });
+
+  function handleUserInput(evt) {
+    setInvestmentObj(prevInvestmentObj => ({
+      ...prevInvestmentObj,
+      [evt.target.name]: +evt.target.value
+    }))
+  }
+
+  console.log(investmentObj);
+  
   return (
     <>
       <Header />
-      <UserInput />
+      <UserInput investmentObj={investmentObj} handleUserInput={handleUserInput} />
     </>
   );
 }
