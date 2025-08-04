8751d385adb9d56d60cdde7feb8e7cbfe8480340
Luong Minh Nhat <luongminhnhat1604@gmail.com>
2025-08-04 17:46:06 +0700

    Fixed wrong import component's name

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import Header from "./components/Header";
-import UserInput from "./components/Userinput";
+import UserInput from "./components/UserInput";
 import Results from "./components/Results";
 
 function App() {
